49ec630c41d119853fc47bacbf38433b1a1542d9
Titulus Desiderio <titulus.web@gmail.com>
2013-07-26 12:46:54 +0300

    crap! i forgot to commit

--- test/tests.js
+++ test/tests.js
@@ -1,43 +1,60 @@
-var testit = {
-    '_first failed': 'test which failed',
-    'passed': 10,
-    'failed': 2,
-    'tests': {
-        'initial' : {
-            'passed': 10,
-            'failed': 2,
-            'tests' : [
-                {'passed':this},
-                {'failed':'aaa'},
-                {'passed':'dsa'}
+
+window.test = new testit;
+
+var mockstack = {
+    status: 'failed',
+    result: {
+        failed: {
+            tests: 2,
+            groups: 1
+        },
+        passed: {
+            tests: 10,
+            groups: 2
+        },
+        total: {
+            tests: 12,
+            groups: 3
+        }
+    },
+    time: 5123,
+    tests: [
+        {
+            type: 'group',
+            name: 'first group',
+            status: 'passed',
+            result: {
+                passed: {
+                    tests: 4,
+                    groups: 0
+                }
+            },
+            tests: [
+                {
+                    type: 'test',
+                    name: 'helloword'
+                }
             ]
         }
-    }
+    ]
 }
+console.log('mockstack: %o',mockstack);
 var tests1=2;
 var tests2=10;
 var group1=2;
 var group2=1;
-console.log('first');
-console.time("overall");
-console.group("%ctestit","color: green;");
-    console.info('%cpassed%c: %d tests in %d groups','color:green;','color:inherit;',(tests1+tests2),(group1+group2));
-    console.timeEnd("overall");
-console.groupEnd();
-console.log(' ');
-console.log('second');
 console.time("overall");
 console.group("%ctestit","color: red;");
     console.warn('%cfailed%c: %d tests in %d groups\n%cpassed%c: %d tests and %d groups','color:red;','color:inherit;',tests1,group2,'color:green;','color:inherit;',tests2,group1);
     console.group("tests");
         console.groupCollapsed("%cinit tests","color: green;");
-            console.log('%cpass%c: first step\n\t%ctestit%c exist\t%o',"color: green;","color: normal;","color: blue;","color: normal;",testit);
-            console.log('%cpass%c: first step\n\t%ctestit%c exist\t%o',"color: green;","color: normal;","color: blue;","color: normal;",testit);
-            console.log('%cpass%c: first step\n\t%ctestit%c exist\t%o',"color: green;","color: normal;","color: blue;","color: normal;",testit);
-            console.log('%cpass%c: first step\n\t%ctestit%c exist\t%o',"color: green;","color: normal;","color: blue;","color: normal;",testit);
+            console.log('%cpass%c: first step\n\t%ctestit%c exist\t%o',"color: green;","color: normal;","color: blue;","color: normal;",test);
+            console.log('%cpass%c: first step\n\t%ctestit%c exist\t%o',"color: green;","color: normal;","color: blue;","color: normal;",test);
+            console.log('%cpass%c: first step\n\t%ctestit%c exist\t%o',"color: green;","color: normal;","color: blue;","color: normal;",test);
+            console.log('%cpass%c: first step\n\t%ctestit%c exist\t%o',"color: green;","color: normal;","color: blue;","color: normal;",test);
         console.groupEnd();
         console.group("%csomthn tests","color: red;");
-            console.log('%cpass%c: first step\n\t%ctestit%c exist\t%o',"color: green;","color: normal;","color: blue;","color: normal;",testit);
+            console.log('%cpass%c: first step\n\t%ctestit%c exist\n\t%O',"color: green;","color: normal;","color: blue;","color: normal;",test);
             console.log('%cfail%c: check smthn\n\t%ctestthat%c not exist',"color: red;","color: normal;","color: blue;","color: normal;");
 
         console.groupEnd();
@@ -47,3 +64,41 @@ console.group("%ctestit","color: red;");
     console.groupEnd();
     console.timeEnd("overall");
 console.groupEnd();
+console.log(1);
+
+// a=true;
+// testmain = test.group('~',function(){
+//         console.count('level');
+//     test.group('asd',function(){
+        
+//         console.count('level');
+//     });
+// });
+
+// test.done(testmain);
+
+// test.record('first record','some text');
+// test.group('first group');
+// test.record('second record','yet another text');
+// test.group('second group',function(){
+//     test.record('first record in second group','how can i do it?');
+//     test.group('subgroup in subgroup',function(){
+//         test.record('first',1);
+//         test.record('second',2);
+//         test.record('last',3);
+//     });
+// });
+test.group('test a',function(){
+    test.it(a);
+        test.comment('try to: a');
+}); test.comment('обернул в группу, что бы отловить ошибку');
+test.group('second a',function(){
+    a = 1+1;
+    test.it(a); test.comment('try to: a; again');
+    test.it(a>5);
+        test.comment('is a>5?');
+    a += 4;
+    test.it(a>5);
+        test.comment('is a>5?');
+});
+test.done();
--- testit.js
+++ testit.js
@@ -1,19 +1,98 @@
-var testit = function(params) {
-    var _it = function(a, b) {
-        var error;
-        try {
-            typeof(a);
-        } catch (exception) {
-            error = exception;
-        } finally {
-            if (error) {
-                console.log('error');
-            } else {
-                console.log('try smthn');
+var testit = function() {
+
+    var group = function() {
+        this.name = undefined;
+        this.status = undefined;
+        this.comment = undefined;
+        this.error = undefined;
+        this.result = {
+            tests: {
+                passed: undefined,
+                failed: undefined,
+                total: undefined
+            },
+            groups: {
+                passed: undefined,
+                failed: undefined,
+                total: undefined
+            }
+        };
+        this.stack = [];
+    }
+
+    var test = function() {
+        this.status = undefined;
+        this.comment = undefined;
+        this.error = undefined;
+    }
+
+    var root = new group();
+    this.root = root;
+    root.name = 'root';
+
+    var _makeGroup = function(name,fun) {
+        var newgroup = new group();
+        newgroup.name = name;
+        newgroup.status ='pass';
+
+        var oldRoot = root;
+        root = newgroup;
+
+        try{fun();} catch(e) {
+            newgroup.status = 'error';
+            newgroup.error = e;
+        }
+        oldRoot.status = (oldRoot.status==='pass' || oldRoot.status===undefined)? root.status: oldRoot.status;
+        root = oldRoot;
+        root.stack.push(newgroup);
+    }
+    this.group = _makeGroup;
+
+    var _it = function(a,b) {
+        var newtest = new test();
+        switch (arguments.length) {
+            case 0 : {
+                newtest.status='error';
+                newtest.error = new ReferenceError("at least one argument expected");
+            } break;
+            case 1 : {
+                if (a) {
+                    newtest.status='pass';
+                } else {
+                    newtest.status='fail';
+                }
+            } break;
+            case 2 : {
+                if (a===b) {
+                    newtest.status='pass';
+                } else {
+                    newtest.status='fail';
+                }
+                newtest.status='pass';
+            } break;
+            default : {
+                newtest.status='error';
+                newtest.error = new RangeError("too much arguments");
             }
         }
+        
+        root.stack.push(newtest);
     }
     this.it = _it;
-}
 
-window.test = new testit();
+    var _comment = function(text) {
+        if (root.stack.length) root.stack[root.stack.length-1].comment = text;
+    }
+    this.comment = _comment;
+
+
+    var _done = function(obj) {
+        // stack.time = +new Date - timestamp;
+        console.dir(root);
+    }
+    this.done = _done;
+
+    var _push = function(tst) {
+
+    }
+}
