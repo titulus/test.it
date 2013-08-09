890afa61c08f5cdb50f27023b15b6d07f57a2ed7
Mikhail Konuhov <piroman@myconfig.ru>
2013-08-10 02:36:13 +0400

    design for loader

---
+++ node/load.js
@@ -0,0 +1,17 @@
+//load jsdom
+//load node-jquery
+//load HttpXMLRequest
+//initialize node
+
+//load test.it
+//load nconsole
+//configure test.it
+
+//load app env from config
+
+//load test.each
+////run test
+
+//output nconsole
+
+//exit with code= count of errors
