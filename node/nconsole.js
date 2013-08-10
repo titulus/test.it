5116483e55899c810a85e8c68bb0b5b82b0065c6
Mikhail Konuyhov <piroman@myconfig.ru>
2013-08-10 08:35:42 +0000

    implement output

--- node/nconsole.js
+++ node/nconsole.js
@@ -1,6 +1,7 @@
 var nconsole = function(){
 
   _buffer = [];
+  _groups = [];
 
   this.log = function(){
     _buffer.push(['log', arguments]);
@@ -19,7 +20,7 @@ var nconsole = function(){
   }
 
   this.dir = function(){
-    //empty
+    _buffer.push(['dir', arguments]);
   }
 
   this.group = function(){
@@ -38,8 +39,37 @@ var nconsole = function(){
     //empty
   }
 
+  _print = function(method, args)
+  {
+    var i;
+    for (i=0; i<args.length; i++){
+      method(args[i]);
+    }
+  }
+
   this.printOutput = function(){
-   console.log(_buffer);
+    var i;
+    for (i=0; i<_buffer.length; i++){
+      switch (_buffer[i][0]) {
+          case 'log':
+              _print(console.log, _buffer[i][1]);
+              break;
+          case 'error':
+              _print(console.error, _buffer[i][1]);
+              break;
+          case 'warn':
+              _print(console.warn, _buffer[i][1]);
+              break;
+          case 'info':
+              _print(console.info, _buffer[i][1]);
+              break;
+          case 'dir':
+              _print(console.dir, _buffer[i][1]);
+              break;
+          default:
+              throw "nconsole.printOutput: not implemented: "+_buffer[i][0];
+      }
+    }
   }
 
 };
