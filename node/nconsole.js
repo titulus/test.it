da548f209fcf429f40f91d45a82f48ee4af41023
Mikhail Konuyhov <piroman@myconfig.ru>
2013-08-10 09:05:15 +0000

    groups internal support

--- node/nconsole.js
+++ node/nconsole.js
@@ -1,26 +1,30 @@
 var nconsole = function(){
 
-  _buffer = [];
-  _groups = [];
+  _buffer = {
+        name: 'console',
+        status: 'opened',
+        entries: [] };
+  _groupsTree = [ _buffer ];
+  _lastEntry = _buffer;
 
   this.log = function(){
-    _buffer.push(['log', arguments]);
+    _lastEntry.entries.push(['log', arguments]);
   }
 
   this.error = function(){
-    _buffer.push(['error', arguments]);
+    _lastEntry.entries.push(['error', arguments]);
   }
 
   this.warn = function(){
-    _buffer.push(['warn', arguments]);
+    _lastEntry.entries.push(['warn', arguments]);
   }
 
   this.info = function(){
-    _buffer.push(['info', arguments]);
+    _lastEntry.entries.push(['info', arguments]);
   }
 
   this.dir = function(){
-    _buffer.push(['dir', arguments]);
+    _lastEntry.entries.push(['dir', arguments]);
   }
 
   this.group = function(){
@@ -39,37 +43,56 @@ var nconsole = function(){
     //empty
   }
 
-  _print = function(method, args)
-  {
+  _print = function(method, args, deep) {
     var i;
     for (i=0; i<args.length; i++){
+      _printDeep(deep, " ");
       method(args[i]);
     }
   }
 
-  this.printOutput = function(){
+  _printDeep = function(deep, symbol) {
     var i;
-    for (i=0; i<_buffer.length; i++){
-      switch (_buffer[i][0]) {
-          case 'log':
-              _print(console.log, _buffer[i][1]);
-              break;
-          case 'error':
-              _print(console.error, _buffer[i][1]);
-              break;
-          case 'warn':
-              _print(console.warn, _buffer[i][1]);
-              break;
-          case 'info':
-              _print(console.info, _buffer[i][1]);
-              break;
-          case 'dir':
-              _print(console.dir, _buffer[i][1]);
-              break;
-          default:
-              throw "nconsole.printOutput: not implemented: "+_buffer[i][0];
-      }
+    for (i=0; i<deep; i++){
+      process.stdout.write(symbol);
     }
+  }
+
+  _printLevel = function(deep, level) {
+     var symbol = "+";
+     if (level.status == 'opened'){
+       symbol = "-";
+     }
+     _printDeep(deep + 1, symbol);
+     process.stdout.write(" " + level.name + "\n");
+     var i;
+     for (i=0; i<level.entries.length; i++){
+       switch (level.entries[i][0]) {
+           case 'log':
+             _print(console.log, level.entries[i][1], deep + 4);
+             break;
+           case 'error':
+             _print(console.error, level.entries[i][1], deep + 4);
+             break;
+           case 'warn':
+             _print(console.warn, level.entries[i][1], deep + 4);
+             break;
+           case 'info':
+             _print(console.info, level.entries[i][1], deep + 4);
+             break;
+           case 'dir':
+             _print(console.dir, level.entries[i][1], deep + 4);
+             break;
+           case 'group':
+             _printLevel(deep+2, level.entries[i][1], deep + 4);
+           default:
+             throw "nconsole.printOutput: not implemented: "+level.entries[i][0];
+       }
+     }
+  }
+
+  this.printOutput = function() {
+    _printLevel(0, _buffer);
   }
 
 };
