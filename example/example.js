8f19cf6e8af4ade5b53f96f63a114d222d4c0138
titulus <titulus.web@gmail.com>
2013-08-14 22:06:54 +0100

    merge tenphi

--- example/example.js
+++ example/example.js
@@ -1,4 +1,4 @@
-console.log( // look how test.typeof() works
+console.log( // look how do test.typeof() work
     test.typeof(1)
    ,test.typeof("text")
    ,test.typeof([1,2,3])
@@ -9,7 +9,7 @@ console.log( // look how test.typeof() works
    ,test.typeof(window)
    ,test.typeof(/yes it is RegExp/));
 
-(function firstFunction() { // look how test.trace() works
+(function firstFunction() { // look how do test.trace() work
     (function secondFunction() {
         (function lastFunction() {
             console.log(test.trace());
@@ -17,7 +17,7 @@ console.log( // look how test.typeof() works
     })();
 })();
 
-var Family = { // Here comes the complex object
+var Family = { // Here some complex object
     name: "Desiderio",
     pet: {
         type: "dog",
@@ -34,35 +34,35 @@ var Family = { // Here comes the complex object
         }
     ]
 }
-var myIQ = 100; // and the number
-var Nothing; // and the empty variable
+var myIQ = 100; // and value
+var Nothing; // and empty value
 
-test.it("Hello world"); // Lets add some simple tests
-test.it(2+2==5).comment("I badly taught algebra at school"); // with comment
-test.it(Infinity>Infinity-1).comment("Philosophically, isn't it?"); // with expression
-// check equality
+test.it("hello world"); // Let"s add some simple tests
+test.it(2+2==5).comment("i badly taught algebra at school"); // with comment
+test.it(Infinity>Infinity-1).comment("philosophically is not it?"); // with expression
+// check equalence
 test.it(myIQ,"genius").comment("Am I a genius?");
-test.it(myIQ,(1+10)*12 - 34 + 5*5*5 - 123).comment("Check if my IQ is OK");
+test.it(myIQ,(1+10)*12 - 34 + 5*5*5 - 123).comment("check my IQ to be a normal");
 // try some chain staff
 if (test.it(Family).comment("Is Family exist? Is it not empty?").result()) {
     console.info("by if: ","Yep! Here it is!");
 } else {
-    console.warn("by if: ","ALARM! there is no Family");
+    console.warn("by if: ","ALARM! there are no Family");
 }
 // do it again in better way
-test.it(Nothing).comment("Is it not empty?").callback(
+test.it(Nothing).comment("Is Nothing exist? Is it not empty?").callback(
     function(){console.info("by callback: ","Yep! Here it is!");}
-   ,function(){console.warn("by callback: ","ALARM! 'Nothing' is empty");});
+   ,function(){console.warn("by callback: ","ALARM! there are no Nothing");});
 
 test.group("Empty group",function(){}); // try to make a group
-test.group('Family tests',function(){ // let's unite them!
-    test.it(Family.name,"Zukerberg").comment("Is it Zukerberg's family?");
+test.group('Family tests',function(){ // let's unite it!
+    test.it(Family.name,"Zukerberg").comment("Do we test Zukerberg's family?");
     test.it(Family.name,"Desiderio").comment("Or Desiderio's?");
 }).comment("unite!");
 test.group("Family tests",function(){ // and add some test after
     test.it(Family.pet).comment("Do we have a pet?")
         .callback(function(){
-            // I add test in your test, so you can test while you test
+            // I add test in your test, so you can test while you testing
             test.it(Family.pet,{type:"dog", name:"google"}).comment("Is it right pet?");
         });
     test.it(Family.house).comment("Do we have a House?")
@@ -74,25 +74,25 @@ test.group("Family tests",function(){ // and add some test after
 test.group("here comes strange tests",function(){
     // test existance of most important Family properties
     test.them([Family.pet, Family.members])
-        .comment("It should contains at least one member with a pet to be called 'a family'");
-    // test type of names
+        .comment("There must be memebers with pet, to call it a 'Family'");
+    // test types of names
     test.types([Family.pet.name, Family.name],"string")
-        .comment("Are names strings?");
-    // here comes some magic
+        .comment("Is names are string type");
+    // here some magic
     var numberOfMembers = test.type(Family.members,"Array")
-        .comment("Are there more than one member here?")
+        .comment("Is it a several members, nor a single member?")
         .arguments()[0].length;
-    test.it(numberOfMembers>5).comment("Is it a big family?");
-    // As we know there are members here, lets check their age
+    test.it(numberOfMembers>5).comment("Is it big family?");
+    // So if we know how many members there, lets check their age
     test.group("Members age",function(){
         for (i=0;i<numberOfMembers;i++) {
             test.it(Family.members[i].age>25)
-                .comment(Family.members[i].name+" should be older than 25");
+                .comment("Is "+Family.members[i].name+" older then 25?");
         }
-        test.it().comment("yep, here is an error"); // add an error to see the trace
+        test.it().comment("yep, here is error"); // add some error to see the trace
     });
 });
-// add a final test deep into groups
+// add final test deep in group
 test.group("here comes strange tests").group("Members age",function(){
     test.it("bye").comment("good");
 });
