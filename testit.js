58593d12654c8acc5fe6a94df2a63986eb984b55
titulus <titulus.web@gmail.com>
2013-09-01 18:03:23 +0100

    fill firebugConsole strategy

--- testit.js
+++ testit.js
@@ -12,14 +12,106 @@ function printerFrom (strategy) {
 }
 
 function firebugConsole(){
-    this.print = function(){
+    /** colors for console.log %c */
+    var green = "color: green",
+        red = "color: red;",
+        orange = "color: orange",
+        blue = "color: blue",
+        normal = "color: normal; font-weight:normal;";
+
+
+    function _print(){
         console.log.apply(console, arguments);
-    }
+    };
+    this.print = _print;
+
+    function _error(error){
+        console.group('%c%s%c: %s',orange,error.type,normal,error.message);
+            if (error.stack) console.log(error.stack);
+            console.dir(error.error);
+        console.groupEnd();
+    };
+    this.error = _error;
+
+    function _test(test){
+        var color = (test.status==='pass')?green:
+                    (test.status==='fail')?red:
+                    (test.status==='error')?orange:normal;
+        
+        var args = ['%c%s%c',color,test.status,normal];
+        
+        if (test.time) {
+            args[0] += ' (%c%s%c ms)';
+            args.push(blue,test.time,normal);
+        };
+
+        if (test.comment) {
+            args[0] += ': %s';
+            args.push(test.comment);
+        }
+
+        if (test.status==='pass') {
+            console.groupCollapsed.apply(console, args);
+        } else {
+            console.group.apply(console, args);
+        }
+
+        if (test.description) console.log(test.description);
+
+        if (test.error) _error(test.error);
+        
+        console.log(test.argument);
+
+        console.groupEnd();
+    };
+    this.test = _test;
+
+    function _group(group) {
+        var color = (group.status==='pass')?green:
+                    (group.status==='fail')?red:
+                    (group.status==='error')?orange:normal;
+
+        var args = ['%s - %c%s%c',group.name,color,group.status,normal];
+
+        args[0] += ' - %c%d%c/%c%d%c/%c%d%c';
+        args.push(green,group.result.pass,normal
+                 ,red,group.result.fail,normal
+                 ,orange,group.result.error,normal);
+
+        args[0] += ' (%c%s%c ms)';
+        args.push(blue,group.time,normal);
+
+        if (group.comment) {
+            args[0] += ': %s';
+            args.push(group.comment);
+        };
+
+        if (group.status==='pass') {
+            console.groupCollapsed.apply(console, args);
+        } else {
+            console.group.apply(console, args);
+        }
+        
+        if (group.description) console.log(group.description);
+
+        if (group.trace) console.log(group.trace);
+
+        for (var i in group.stack) {
+            (group.stack[i].type==='test')?_test(group.stack[i]):_group(group.stack[i]);
+        }
+
+        if (group.error) _error(group.error);
+
+        console.groupEnd();
+
+    };
+    this.group = _group;
 }
 
 var printer = new printerFrom(new firebugConsole);
 
 scope.printer = printer;
+
 
 function Testit () {
     /**
@@ -585,6 +677,7 @@ function Testit () {
      * @private
      */
     function _done() {
+
         /** update time in root */
         if (!rootTimeDone && root.name==='root') {
             root.time = new Date().getTime() - root.time;
@@ -592,10 +685,13 @@ function Testit () {
         }
 
         var curentLevel = (this.link)?this.link:root;
+        console.warn(this.link);
+        console.warn(root);
+        console.warn(curentLevel);
 
         /** display result */
-        printer.group(curentLevel);
-        // _printConsole(curentLevel);
+        // printer.group(curentLevel);
+        printConsole(curentLevel);
     }
     /**
      * public interface for _done()
