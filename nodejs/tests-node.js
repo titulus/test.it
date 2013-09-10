9a52ee47e0c007600323a5e5f05a810cd4bcd464
Titulus Desiderio <titulus.web@gmail.com>
2013-09-10 14:05:46 +0300

    move nodejs printer and example to new folder `nodejs`

---
+++ nodejs/tests-node.js
@@ -0,0 +1,23 @@
+(test = require('../testit.js')).printer(require('../testit-nodejs.js'));
+
+test.it(true)
+    .comment('passed test');
+test.it(false)
+    .comment('failed test');
+test.it()
+    .comment('error test');
+test.group('first group',function(){
+    test.it(true)
+        .comment('single test');
+
+    test.group('nested group',function(){
+        test.group('deep nested group',function(){
+            test.group('very deep nested group',function(){
+                test.it(true)
+                    .comment('another simple test');
+            });
+        });
+    });
+}).comment('consist one test and one group');
+
+test.done();
