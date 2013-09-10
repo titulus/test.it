f5434310cd467cf80c9de205b0520269961f0b75
Titulus Desiderio <titulus.web@gmail.com>
2013-09-10 14:07:43 +0300

    rename some files in example

---
+++ browser/example.js
@@ -0,0 +1,21 @@
+test.it(true)
+    .comment('passed test');
+test.it(false)
+    .comment('failed test');
+test.it()
+    .comment('error test');
+test.group('first group',function(){
+    test.it(true)
+        .comment('single test');
+
+    test.group('nested group',function(){
+        test.group('deep nested group',function(){
+            test.group('very deep nested group',function(){
+                test.it(true)
+                    .comment('another simple test');
+            });
+        });
+    });
+}).comment('consist one test and one group');
+
+test.done();
